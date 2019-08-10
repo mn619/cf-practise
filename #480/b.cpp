fce26fdd5c9a514df7e55b6266e6cb0c3caba45a
Aman <adubey@ma.iitr.ac.in>
2019-08-10 13:07:28 +0530

    Add contest

---
+++ #480/b.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+
+using namespace std;
+
+int dp[501] = {1};
+
+signed main()
+{
+	string s;
+	cin>>s;
+
+	for(int i = 0; i < s.size(); i++){
+		
+	}	
+}
